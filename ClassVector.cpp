b913232f50c2bb36ccbd6e014831411952b37b88
AlexAlvaro <38161842+AlexAlvaro@users.noreply.github.com>
2018-09-06 07:14:49 -0500

    Jueves

--- ClassVector.cpp
+++ ClassVector.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
-#include "my_vector.h"
+#include <string>
+#include "vect.h"
+using namespace std;
+int main(){
+  Vector<int> datosEntero;
+  Vector<int> datoCadena;
+  for (int i =0;i<10;i++){
+    datosEntero.push_back(i);
+    datoCadena.push_back("palabra");
+  }
+  for (int i =0;i<10;i++){
+    cout<<datosEntero.get(i);
+    cout<<datoCadena.get("palabra");
+  }
 
-int main()
+  return 0;
+}
