d8b735283b058d3fb91e7bccbdbb001679f7b1af
AlexAlvaro <38161842+AlexAlvaro@users.noreply.github.com>
2018-09-06 07:13:58 -0500

    jueves

--- my_vector.h
+++ my_vector.h
@@ -1,31 +1,48 @@
 #ifndef MY_VECTOR_H
 #define MY_VECTOR_H
+#include <iostream>
+#include <string>
+using namespace std;
 
 template <class T>
-class my_vector{
+class Vector{
   private:
-    int m_size;
-    T *p_data;
     int MAX_CAP;
+    int m_size;
+    t *p_data;
     static const int MAX;
   public:
-    vector(int a_size=MAX);
-    ~vector();
-    void push_back();
+    Vector(int a_size=MAX);
+    ~Vector();
+    int size()const;
+    void push_back(const T &obj);
+    T& get(int index);
 };
 template <class T>
-  int my_vector<T>::MAX=10;
-
+Vector<T>::Vector(int a_size=MAX){
+    MAX=10;
+    m_size=0;
+    p_data=new T(a_size);
+    //this->p_data=new T[this<>cap]
+}
 template <class T>
-  my_vector::vector(int a_size){
-    p_data= new T(a_size);
-  }
+Vector<T>::~vector(){
+    delete[] this->p_data;
 
+}
+template <class T>
+int Vector<T>::size()const{
+    return this->m_size;
+}
+template <class T>
+void Vector <T>:: push_back(const &T){
+    if(this->m_size<this->cap)
+      this->p_data[this->m_size++]=obj;
 
-
-
-
-
-
-
+}
+template <class T>
+T& Vector <T>::get(int index){
+    if(index <0||index>=this->m_size)
+      return this->p_data[index];
+}
 #endif //MY_VECTOR_HS
